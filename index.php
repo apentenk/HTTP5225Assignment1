c9781b22aee0ec51a1c7ee5b3f465dba5520df4a
Kwasi Apenteng <kwasi.a.apenteng@gmail.com>
2024-07-04 15:41:23 -0400

    updated index with metadata

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php include ('./inc/functions.php') ?>
 <!doctype html>
-<html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assignment 1 - PHP and MySQL</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
